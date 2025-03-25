c9cee0bfb1cbc7f84c9eb9ec24b0f7fd9e826257
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-03-25 20:51:39 +0100

    prueba pipeline dockerFile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editado por Carlota para prueba de docker2
+          Editado por Carlota para prueba de docker prueba 3
         </p>
         <a
           className="App-link"
